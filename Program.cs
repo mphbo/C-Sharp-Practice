e4cf9f1c85c2f5c698fe7f816860a14b98607b5f
Logan <logannormanthomas@protonmail.com>
2021-07-25 17:36:43 -0700

    completed logical comparisons

--- Program.cs
+++ Program.cs
@@ -6,16 +6,11 @@ namespace Variables
   {
     static void Main(string[] args)
     {
-      try
-      {
-        string str = "true";
-        bool b = Convert.ToBoolean(str);
-        Console.WriteLine(b);
-      }
-      catch (Exception)
-      {
-         Console.WriteLine("The number could not be converted to a byte.");
-      }
+      var a = 1;
+      var b = 2;
+      var c = 3;
+
+      Console.WriteLine(!(c > b || c == a));
     }
   }
 }
