d1400f24ac7015b10296d50bde49accf5c73460e
Logan <logannormanthomas@protonmail.com>
2021-07-26 16:38:49 -0700

    completed mini calculator app with Write()

--- Program.cs
+++ Program.cs
@@ -6,11 +6,12 @@ namespace Variables
   {
     static void Main(string[] args)
     {
-      Console.Write("Enter your name: ");
-      string name = Console.ReadLine();
-      Console.Write("Enter your age: ");
-      string age = Console.ReadLine();
-      Console.WriteLine("Hello " + name + " you are " + age);
+      Console.Write("Enter a number: ");
+      double num1 = Convert.ToDouble(Console.ReadLine());
+      Console.Write("Enter another number: ");
+      double num2 = Convert.ToDouble(Console.ReadLine());
+
+      Console.WriteLine(num1 + num2);
     }
   }
 }
