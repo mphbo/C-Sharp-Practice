377001dca6e61af213a931dd8d5738eff2e4a3ee
Logan <logannormanthomas@protonmail.com>
2021-07-26 16:47:23 -0700

    completed a mad libs game on the command line

--- Program.cs
+++ Program.cs
@@ -6,12 +6,20 @@ namespace Variables
   {
     static void Main(string[] args)
     {
-      Console.Write("Enter a number: ");
-      double num1 = Convert.ToDouble(Console.ReadLine());
-      Console.Write("Enter another number: ");
-      double num2 = Convert.ToDouble(Console.ReadLine());
+      string color, pluralNoun, celebrity;
 
-      Console.WriteLine(num1 + num2);
+      Console.Write("Enter a color: ");
+      color = Console.ReadLine();
+
+      Console.Write("Enter a plural noun: ");
+      pluralNoun = Console.ReadLine();
+
+      Console.Write("Enter a celebrity: ");
+      celebrity = Console.ReadLine();
+
+      Console.WriteLine("Roses are " + color);
+      Console.WriteLine(pluralNoun + " are blue");
+      Console.WriteLine("I Love " + celebrity);
     }
   }
 }
