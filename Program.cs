690e5036b356e88d6454947e279210a0a182baf7
Logan <logannormanthomas@protonmail.com>
2021-07-25 17:28:08 -0700

    completed operators section

--- Program.cs
+++ Program.cs
@@ -6,8 +6,16 @@ namespace Variables
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
-      Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+      try
+      {
+        string str = "true";
+        bool b = Convert.ToBoolean(str);
+        Console.WriteLine(b);
+      }
+      catch (Exception)
+      {
+         Console.WriteLine("The number could not be converted to a byte.");
+      }
     }
   }
 }
