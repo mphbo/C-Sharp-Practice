296f59cf6442e992124001995819ebee0e120442
Logan <logannormanthomas@protonmail.com>
2021-07-26 21:35:59 -0700

    learned how to do while/do while loops

--- Program.cs
+++ Program.cs
@@ -7,47 +7,14 @@ namespace Variables
     static void Main(string[] args)
     {
 
-      Console.WriteLine("Pick a number representing a day of the week.");
-      int dayNum = Convert.ToInt16(Console.ReadLine());
-      Console.WriteLine(GetDay(dayNum));
-
-    }
-
-    static string GetDay(int dayNum) 
-    {
-      string dayName;
-
-      switch (dayNum)
+      int index = 6;
+      do
       {
-        case 0:
-          dayName = "Sunday";
-          break;
-        case 1:
-          dayName = "Monday";
-          break;
-        case 2:
-          dayName = "Wednesday";
-          break;
-        case 3:
-          dayName = "Thursday";
-          break;
-        case 4:
-          dayName = "Friday";
-          break;
-        case 5:
-          dayName = "Saturday";
-          break;
-        case 6:
-          dayName = "Sunday";
-          break;
-        default:
-          dayName = "Invalid Day Number";
-          break;
-      }
+        Console.WriteLine(index);
+        index++;
+      } while (index <= 5);
 
-      return dayName;
     }
-
 
   }
 }
